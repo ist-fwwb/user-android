2eac8d3454bccef2415c4cbf6c8bb719a56c4043
Tobb Huang <tobbhuang@gmail.com>
2018-12-25 16:55:40 +0800

    init

---
+++ app/src/main/java/com/huangtao/user/mvp/MvpLazyFragment.java
@@ -0,0 +1,34 @@
+package com.huangtao.user.mvp;
+
+import com.huangtao.user.common.MyLazyFragment;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/11/17
+ *    desc   : MVP 懒加载 Fragment 基类
+ */
+public abstract class MvpLazyFragment<P extends MvpPresenter> extends MyLazyFragment implements IMvpView {
+
+    private P mPresenter;
+
+    @Override
+    protected void initFragment() {
+        mPresenter = createPresenter();
+        mPresenter.attach(this);
+        mPresenter.start();
+        super.initFragment();
+    }
+
+    @Override
+    public void onDestroy() {
+        mPresenter.detach();
+        super.onDestroy();
+    }
+
+    public P getPresenter() {
+        return mPresenter;
+    }
+
+    protected abstract P createPresenter();
+}
---
+++ app/src/main/java/com/huangtao/user/ui/activity/AboutActivity.java
@@ -0,0 +1,33 @@
+package com.huangtao.user.ui.activity;
+
+import com.huangtao.user.R;
+import com.huangtao.user.common.MyActivity;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 关于界面
+ */
+public class AboutActivity extends MyActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_about;
+    }
+
+    @Override
+    protected int getTitleBarId() {
+        return R.id.tb_about_title;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
