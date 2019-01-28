a72b795bf4143dd18c148614df3676413e943ea7
ssingularity <1755405701@qq.com>
2019-01-28 21:42:32 +0800

    modify the server address

--- app/src/main/java/com/huangtao/user/network/Network.java
+++ app/src/main/java/com/huangtao/user/network/Network.java
@@ -14,7 +14,7 @@ public class Network {
                     //设置数据解析器
                     .addConverterFactory(GsonConverterFactory.create())
                     //设置网络请求的Url地址
-                    .baseUrl("http://129.211.119.51:31000/")
+                    .baseUrl("http://47.106.8.44:31000/")
 //                    .baseUrl("http://192.168.0.102:8080/")
                     .build();
             // 创建网络请求接口的实例
